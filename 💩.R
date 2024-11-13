0d7e4c5bd0ace6197bea69ba3d380840e9425431
Yijia Lin <127508270+Yicaa@users.noreply.github.com>
2024-11-13 16:29:44 +0100

    v2

--- 💩.R
+++ 💩.R
@@ -2,3 +2,5 @@ library(dplyr)
 data <- starwars |>  filter(mass > 60) |> select(name, mass, height)
 library(readr)
 write_csv(data, file = "~/Documents/data-programming/TASK0 - INTRO R 2/starwarss.csv")
+
+data |> mutate("bmi"=mass/height^2)
